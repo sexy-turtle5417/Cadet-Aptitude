432d1de0e9c9c2afb4008f37c54d2b720d6dcd00
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-16 11:29:52 +0800

    feat: Add FieldSet component

---
+++ app/View/Components/FieldSet.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class FieldSet extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $legend,
+        public string $label = ""
+    ) {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.field-set', [
+            "legend" => $this->legend,
+            "label" => $this->label
+        ]);
+    }
+}
---
+++ resources/views/components/field-set.blade.php
@@ -0,0 +1,5 @@
+<fieldset class="fieldset">
+    <legend class="fieldset-legend font-bold">{{ $legend }}</legend>
+    {{ $slot }}
+    <p class="label">{{ $label }}</p>
+</fieldset>
